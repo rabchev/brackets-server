a3215132f9d89643d27779b98206002594bfbbb6
Boyan Rabchev <boyan@rabchev.com>
2012-10-24 01:07:09 +0300

    merge from bare repo

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var PI = Math.PI;
-
-exports.area = function (r) {
-  return PI * r * r;
-};
-
-exports.circumference = function (r) {
-  return 2 * PI * r;
-};
+module.exports = require('./lib/node-brackets.js');
