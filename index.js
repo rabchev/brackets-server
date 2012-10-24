f316aa7f0f6be755d6c3b66bff3a855ef677e700
Boyan Rabchev <boyan@rabchev.com>
2012-10-25 00:24:44 +0300

    Removed "send" module, using connect's "send" module.

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-module.exports = require('./lib/node-brackets.js');
-=======
-module.extports = require('./lib/node-brackets.js');
->>>>>>> b154867e8fd942f7b268c4d5cd53a89790fa2c8b
-=======
 module.exports = require('./lib/node-brackets');
->>>>>>> 815f1eeae0bfefe5845926f0b5d854cb097fccd6
--- test/test-web-app.js
+++ test/test-web-app.js
@@ -3,9 +3,9 @@ var testCase  = require('nodeunit').testCase;
 var connect = require('connect');
 var brackets = require('../lib/node-brackets.js');
 var http = require("http");
+var port = 3119;
+var appUrl = "http://localhost:" + port;
 var app;
-
-console.log('Listening on port 3000');
 
 module.exports = testCase({
     "Fixture Setup": function(test) {
@@ -14,13 +14,13 @@ module.exports = testCase({
             .use('/*', function(req, res){
                   res.send('Hello World');
                 })
-            .listen(3000);
+            .listen(port);
         
         test.done();
     },
-    "test 1": function(test) {
+    "test index.html": function(test) {
         test.expect(1);
-        http.get("http://localhost:3000/brackets", function(res) {
+        http.get(appUrl + "/brackets", function(res) {
             test.equal(res.statusCode, 200);
             test.done();
         }).on('error', function(e) {
