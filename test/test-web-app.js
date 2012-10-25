e651c3e4fa6ae0ca64197bf6191fc28b1a4389bd
Boyan Rabchev <boyan@rabchev.com>
2012-10-25 23:46:57 +0300

    More tests

--- test/test-web-app.js
+++ test/test-web-app.js
@@ -26,8 +26,42 @@ module.exports = testCase({
             test.equal(res.statusCode, 200);
             test.equal(res.headers["content-type"], "text/html; charset=UTF-8");
             res.on("data", function(chunk) {
-                var str = "" + chunk;
+                var str = String(chunk);
                 test.ok(str.indexOf("<script src=\"thirdparty/require.js\" data-main=\"brackets\"></script>") != -1);
+                test.done();
+            });            
+        }).on('error', function(e) {
+            console.log("Got error: " + e.message);
+            test.done();
+        });
+    },
+    "test resource file": function(test) {
+        test.expect(3);
+        http.get(appUrl + "/brackets/thirdparty/require.js", function(res) {
+            res.setEncoding('utf8');
+            
+            test.equal(res.statusCode, 200);
+            test.equal(res.headers["content-type"], "application/javascript");
+            res.on("data", function(chunk) {
+                var str = String(chunk);
+                test.ok(str.indexOf("RequireJS 1.0.3 Copyright (c) 2010-2011, The Dojo Foundation All Rights Reserved.") != -1);
+                test.done();
+            });            
+        }).on('error', function(e) {
+            console.log("Got error: " + e.message);
+            test.done();
+        });
+    },
+    "test non Brackets request": function(test) {
+        test.expect(3);
+        http.get(appUrl + "/", function(res) {
+            res.setEncoding('utf8');
+            
+            test.equal(res.statusCode, 200);
+            test.equal(res.headers["content-type"], "text/html");
+            res.on("data", function(chunk) {
+                var str = String(chunk);
+                test.ok(str.indexOf("Hello World") != -1);
                 test.done();
             });            
         }).on('error', function(e) {
