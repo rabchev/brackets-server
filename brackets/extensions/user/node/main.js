1f30ac511327364b86d404bfcbd323eb5b465cc6
Boyan Rabchev <boyan@rabchev.com>
2012-10-27 00:46:01 +0300

    Removed Bracktes source, will add it as git submodule.

--- brackets/extensions/user/node/main.js
+++
@@ -1,7 +0,0 @@
-/*jslint plusplus: true, devel: true, nomen: true, node: true, indent: 4, maxerr: 50 */
-/*global define, require, exports, module */
-
-define(function (require, exports, module) {
-    "use strict";
-    console.log("node init........");
-});
