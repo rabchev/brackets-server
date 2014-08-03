d448d6ee4a255e75dd43dd052c85c3aefeb3067d
Boyan Rabchev <boyan@rabchev.com>
2014-08-04 01:28:35 +0300

    socket-io-fs implementation

--- hacks/app.js
+++ hacks/app.js
@@ -7,5 +7,34 @@ define(function (require, exports) {
         return "/";
     };
 
+    /**
+     * Returns the TCP port of the current Node server
+     *
+     * @param {function(err, port)} callback Asynchronous callback function. The callback gets two arguments
+     *        (err, port) where port is the TCP port of the running server.
+     *        Possible error values:
+     *         ERR_NODE_PORT_NOT_SET      = -1;
+     *         ERR_NODE_NOT_RUNNING       = -2;
+     *         ERR_NODE_FAILED            = -3;
+     *
+     * @return None. This is an asynchronous call that sends all return information to the callback.
+     */
+    exports.getNodeState = function (callback) {
+        callback(-2);
+    };
 
+
+    var Fn = Function, global = (new Fn("return this"))();
+    if (!global.Mustache.compile) {
+        global.Mustache.compile = function (template) {
+            // This operation parses the template and caches
+            // the resulting token tree. All future calls to
+            // mustache.render can now skip the parsing step.
+            global.Mustache.parse(template);
+
+                return function (view, partials) {
+                return global.Mustache.render(template, view, partials);
+            };
+        };
+    }
 });
