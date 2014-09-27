0bc50181c6aed632437b50e95eca6552a530af91
Boyan Rabchev <boyan@rabchev.com>
2014-09-27 03:03:58 +0300

    Added command line artguments for projects and supprot dirs.

--- bin/run.js
+++ bin/run.js
@@ -3,15 +3,22 @@
 var commander   = require("commander"),
     brackets    = require("../"),
     pkg         = require("../package.json"),
-    open        = require("open");
+    open        = require("open"),
+    path        = require("path"),
+    homeDir     = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE || process.cwd();
 
 commander
     .version(pkg.version)
-    .option("-p, --port <port>", "Specifies TCP <port> for Brackets service. If omitted, the first free port in the range of 6000 - 6800 is picked.")
-    .option("-o, --open", "Opens the project in the default web browser. Warning: since Brackets currently supports only Chrome you should set it as your default browser.")
+    .option("-p, --port <port>", "Specifies TCP <port> for Brackets service. The default port is 6800.")
+    .option("-o, --open", "Opens Brackets in the default web browser.")
+    .option("-s, --supp-dir", "Specifies the root directory for Brackets supporting files such as user extensions, configurations and state persistence. The default locations is ~/.brackets-srv.")
+    .option("-j, --proj-dir", "Specifies the root directory for projects. The default locations is ~/Projects.")
     .parse(process.argv);
 
-var app = brackets(commander.port);
+var app = brackets(commander.port, {
+    supportDir: commander["supp-dir"] || path.join(homeDir, ".brackets-srv"),
+    projectsDir: commander["proj-dir"] || path.join(homeDir, "Projects")
+});
 
 if (commander.open) {
     open("http://localhost:" + app.httpServer.address().port);
